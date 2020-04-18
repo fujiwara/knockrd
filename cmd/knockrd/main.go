60d14036ed1e7800d471fcb575cc41f2a48fb53b
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2020-04-18 16:09:45 +0900

    debug logging

--- cmd/knockrd/main.go
+++ cmd/knockrd/main.go
@@ -3,14 +3,36 @@ package main
 import (
 	"flag"
 	"log"
+	"os"
+	"strings"
 
 	"github.com/fujiwara/knockrd"
+	"github.com/hashicorp/logutils"
 )
+
+var filter = &logutils.LevelFilter{
+	Levels:   []logutils.LogLevel{"debug", "info", "warn", "error"},
+	MinLevel: logutils.LogLevel("info"),
+	Writer:   os.Stderr,
+}
 
 func main() {
 	var configFile string
+	var debug bool
 	flag.StringVar(&configFile, "config", "", "config file name")
+	flag.BoolVar(&debug, "debug", false, "enable debug log")
+	flag.VisitAll(func(f *flag.Flag) {
+		if s := os.Getenv(strings.ToUpper("KNOCKRD_" + f.Name)); s != "" {
+			f.Value.Set(s)
+		}
+	})
 	flag.Parse()
+
+	if debug {
+		filter.MinLevel = logutils.LogLevel("debug")
+	}
+	log.SetOutput(filter)
+
 	cfg, err := knockrd.LoadConfig(configFile)
 	if err != nil {
 		log.Fatal(err)
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.14
 require (
 	github.com/aws/aws-sdk-go v1.30.8
 	github.com/guregu/dynamo v1.7.0
+	github.com/hashicorp/logutils v1.0.0
 	github.com/kayac/go-config v0.3.1
 	github.com/natureglobal/realip v0.0.1
 )
